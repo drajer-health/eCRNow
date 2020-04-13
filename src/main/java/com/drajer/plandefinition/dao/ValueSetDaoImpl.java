f4364a9907ef4769f7d6d36a8588cf890a62d9a1
monikadrajer <mounika.narne@drajer.com>
2020-04-14 03:41:29 +0530

    committing new table changes

--- src/main/java/com/drajer/plandefinition/dao/ValueSetDaoImpl.java
+++ src/main/java/com/drajer/plandefinition/dao/ValueSetDaoImpl.java
@@ -2,15 +2,14 @@ package com.drajer.plandefinition.dao;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-import javax.transaction.Transactional.TxType;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.drajer.plandefinition.model.ValueSetGrouperModel;
 import com.drajer.plandefinition.model.ValueSetModel;
+import com.drajer.plandefinition.model.planDefinitionActionModel;
 
 @Repository("valueSetDaoImpl")
 public class ValueSetDaoImpl extends AbstractDao implements ValueSetDao {
@@ -31,12 +30,32 @@ public class ValueSetDaoImpl extends AbstractDao implements ValueSetDao {
 	}
 
 	@Override
-	@Transactional(value = TxType.REQUIRES_NEW)
 	public void createValueset(ValueSetModel valueSetModel) {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(valueSetModel);
 		session.getTransaction().commit();
+		session.close();
+	}
+
+	@Override
+	public void createValuesetGrouper(ValueSetGrouperModel valuesetGrouper) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(valuesetGrouper);
+		session.getTransaction().commit();
+		session.close();
+		
+	}
+
+	@Override
+	public void createPlanDefinitionActions(planDefinitionActionModel planDefinitionAction) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(planDefinitionAction);
+		session.getTransaction().commit();
+		session.close();
+		
 	}
 
 }
---
+++ src/main/java/com/drajer/plandefinition/model/ValueSetGrouperModel.java
@@ -0,0 +1,49 @@
+package com.drajer.plandefinition.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "valuesetgrouper")
+public class ValueSetGrouperModel {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+
+	@Column(name = "valuesetgrouper")
+	private String valueSetGrouper;
+
+	@Column(name = "valuesetid")
+	private String valueSetId;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getValueSetGrouper() {
+		return valueSetGrouper;
+	}
+
+	public void setValueSetGrouper(String valueSetGrouper) {
+		this.valueSetGrouper = valueSetGrouper;
+	}
+
+	public String getValueSetId() {
+		return valueSetId;
+	}
+
+	public void setValueSetId(String valueSetId) {
+		this.valueSetId = valueSetId;
+	}
+
+}
