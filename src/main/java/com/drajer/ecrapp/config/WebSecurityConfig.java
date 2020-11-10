4383b58d0ebf783da607a1c6fe24e4916cb1ee48
nm036574 <Nigel.Meachen@Cerner.com>
2020-11-10 12:37:58 -0500

    Adjust WebSecurityConfig to match more than just POST methods.

--- src/main/java/com/drajer/ecrapp/config/WebSecurityConfig.java
+++ src/main/java/com/drajer/ecrapp/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -40,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           .addFilterAfter(
               (Filter) classInstance.newInstance(), UsernamePasswordAuthenticationFilter.class)
           .authorizeRequests()
-          .antMatchers(HttpMethod.POST, "/api/**")
+          .antMatchers("/api/**")
           .permitAll()
           .anyRequest()
           .authenticated();
