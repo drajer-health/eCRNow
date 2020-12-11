6c95ce72b327a1772dc5f66d7771f3025ea124df
nbashyam <nagesh_bashyam@yahoo.com>
2020-12-11 23:18:29 +0530

    Fixed Path Manipulation issue

--- src/main/java/com/drajer/ecrapp/util/ApplicationUtils.java
+++ src/main/java/com/drajer/ecrapp/util/ApplicationUtils.java
@@ -457,4 +457,15 @@ public class ApplicationUtils {
     }
     return bundle;
   }
+
+  public static String getRandomString(int n) {
+    String AlphaNumericString =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+    StringBuilder sb = new StringBuilder(n);
+    for (int i = 0; i < n; i++) {
+      int index = (int) (AlphaNumericString.length() * Math.random());
+      sb.append(AlphaNumericString.charAt(index));
+    }
+    return sb.toString();
+  }
 }
