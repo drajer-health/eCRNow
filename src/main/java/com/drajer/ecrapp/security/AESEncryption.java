ec19f3e931e11815ef267c2c616b5e5707e115e1
nbashyam <nagesh_bashyam@yahoo.com>
2020-12-11 17:45:22 +0530

    Fixed Encryption issues

--- src/main/java/com/drajer/ecrapp/security/AESEncryption.java
+++ src/main/java/com/drajer/ecrapp/security/AESEncryption.java
@@ -26,7 +26,7 @@ public class AESEncryption {
     MessageDigest sha = null;
     try {
       key = myKey.getBytes("UTF-8");
-      sha = MessageDigest.getInstance("SHA-1");
+      sha = MessageDigest.getInstance("SHA-256");
       key = sha.digest(key);
       key = Arrays.copyOf(key, 16);
       secretKey = new SecretKeySpec(key, "AES");
