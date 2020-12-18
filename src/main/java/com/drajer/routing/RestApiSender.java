afa6d611490a62963c7fd71c20ede48211606658
Jesse Wyatt <jesse.wyatt@cerner.com>
2020-12-18 10:55:03 -0600

    remove qualifier

--- src/main/java/com/drajer/routing/RestApiSender.java
+++ src/main/java/com/drajer/routing/RestApiSender.java
@@ -7,7 +7,6 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -21,8 +20,7 @@ public class RestApiSender {
 
   private final Logger logger = LoggerFactory.getLogger(RestApiSender.class);
 
-  @Autowired
-  private AuthorizationService authorizationService;
+  @Autowired private AuthorizationService authorizationService;
 
   @Autowired private RestTemplate restTemplate;
 
