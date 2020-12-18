84cb529e2f1b3e09b74146f38d1959f961bec152
Jesse Wyatt <jesse.wyatt@cerner.com>
2020-12-18 12:27:06 -0600

    sync up

--- src/main/java/com/drajer/routing/RestApiSender.java
+++ src/main/java/com/drajer/routing/RestApiSender.java
@@ -7,7 +7,6 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
