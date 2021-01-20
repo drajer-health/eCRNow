4f3e49df80e78a6026096d0c5e20ef5963875424
nbashyam <nagesh_bashyam@yahoo.com>
2021-01-20 19:34:59 +0530

    Bug fixes

--- src/main/java/com/drajer/ecrapp/security/SampleAuthorizationServiceImpl.java
+++ src/main/java/com/drajer/ecrapp/security/SampleAuthorizationServiceImpl.java
@@ -5,6 +5,7 @@ import java.security.SecureRandom;
 import java.util.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 
 public class SampleAuthorizationServiceImpl implements SampleAuthorizationService {
 
@@ -14,27 +15,13 @@ public class SampleAuthorizationServiceImpl implements SampleAuthorizationServic
 
   private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
 
-  public String getAuthorizationHeader(LaunchDetails launchDetails) {
+  public HttpHeaders getAuthorizationHeader(LaunchDetails launchDetails) {
+    HttpHeaders headers = new HttpHeaders();
     logger.info("Generating the new AccessToken");
-    /*
-     * RestTemplate restTemplate = new RestTemplate(); JSONObject requestBody = new
-     * JSONObject(); requestBody.put("client_id", launchDetails.getClientId());
-     * requestBody.put("client_secret", launchDetails.getClientSecret());
-     * HttpHeaders headers = new HttpHeaders(); headers.add("Content-Type",
-     * MediaType.APPLICATION_JSON_VALUE); HttpEntity<String> request = new
-     * HttpEntity<>(requestBody.toString(), headers);
-     *
-     * logger.info("Sending Authorization request to Endpoint::::: {}",
-     * launchDetails.getRestAPIURL()); ResponseEntity<String> response =
-     * restTemplate.exchange( launchDetails.getRestAPIURL(), HttpMethod.POST,
-     * request, String.class);
-     *
-     * JSONObject responseObj = new JSONObject(response.getBody()); accessToken =
-     * responseObj.getString("access_token");
-     * logger.info("received Access_token====> {}", accessToken);
-     */
     byte[] randomBytes = new byte[24];
     secureRandom.nextBytes(randomBytes);
-    return base64Encoder.encodeToString(randomBytes);
+    // Set Authorization Header
+    headers.add("Authorization", "Bearer " + base64Encoder.encodeToString(randomBytes));
+    return headers;
   }
 }
