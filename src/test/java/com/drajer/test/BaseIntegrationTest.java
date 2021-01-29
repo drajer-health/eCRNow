ed34c76a6cf281fcf6a75bee273afb5e199f2ac9
Venkatachalaiah Anantha <anantha.venkatachalaiah@cerner.com>
2021-01-29 18:04:33 +0530

    Added RR tests

--- src/test/java/com/drajer/test/BaseIntegrationTest.java
+++ src/test/java/com/drajer/test/BaseIntegrationTest.java
@@ -107,7 +107,7 @@ public abstract class BaseIntegrationTest {
 
     // Hardcode FHIR & Token URL to avoid mistakes in test data file.
     String fhirUrl = URL + wireMockHttpPort + fhirBaseUrl;
-    String authUrl = fhirBaseUrl + "/token";
+    String authUrl = fhirUrl + "/token";
     String directUrl = URL + wireMockHttpPort + "/directurl";
     JSONObject jsonObject = new JSONObject(clientDetailString);
     jsonObject.put("fhirServerBaseURL", fhirUrl);
