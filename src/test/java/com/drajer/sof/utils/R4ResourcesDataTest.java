35884cfd648b650c64a8a1c0707a16db70971845
Anand <SA070622@cerner.net>
2021-02-01 09:37:35 +0530

    Unit Tests

---
+++ src/test/java/com/drajer/sof/utils/R4ResourcesDataTest.java
@@ -0,0 +1,21 @@
+package com.drajer.sof.utils;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.hl7.fhir.r4.model.DocumentReference;
+import org.junit.Test;
+
+import com.drajer.test.util.TestUtils;
+
+public class R4ResourcesDataTest {
+	
+	@Test
+	public void constructR4DocumentReferenceTest() {
+		R4ResourcesData dataObject=new R4ResourcesData();
+		String rrXml=TestUtils.getFileContentAsString("R4/Misc/TestRRXml.xml");
+		DocumentReference result= dataObject.constructR4DocumentReference(rrXml, "P123456", "E98765");
+		assertNotNull(result);
+	}
+
+
+}
