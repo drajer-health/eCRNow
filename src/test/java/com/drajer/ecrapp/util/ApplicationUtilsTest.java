ff0aece26b6c51ed923619b7018d9b80c7c6b59d
Venkatachalaiah Anantha <anantha.venkatachalaiah@cerner.com>
2021-03-02 17:50:40 +0530

    Added test validation

--- src/test/java/com/drajer/ecrapp/util/ApplicationUtilsTest.java
+++ src/test/java/com/drajer/ecrapp/util/ApplicationUtilsTest.java
@@ -1,8 +1,11 @@
 package com.drajer.ecrapp.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import org.hl7.fhir.r4.model.Duration;
 import org.junit.Test;
 
@@ -13,10 +16,31 @@ public class ApplicationUtilsTest {
 
     Duration d = new Duration();
     d.setUnit("s");
-    d.setValue(12345);
+    d.setValue(10);
 
-    Instant instant = ApplicationUtils.convertDurationToInstant(d);
-    assertNotNull(instant);
+    Instant expectedInstant = new Date().toInstant().plusSeconds(d.getValue().longValue());
+    Instant actualInstant = ApplicationUtils.convertDurationToInstant(d);
+
+    assertNotNull(actualInstant);
+    assertEquals(
+        expectedInstant.truncatedTo(ChronoUnit.SECONDS),
+        actualInstant.truncatedTo(ChronoUnit.SECONDS));
+  }
+
+  @Test
+  public void testConvertDurationToInstant_sec_upperCase() {
+
+    Duration d = new Duration();
+    d.setUnit("S");
+    d.setValue(10);
+
+    Instant expectedInstant = new Date().toInstant().plusSeconds(d.getValue().longValue());
+    Instant actualInstant = ApplicationUtils.convertDurationToInstant(d);
+
+    assertNotNull(actualInstant);
+    assertEquals(
+        expectedInstant.truncatedTo(ChronoUnit.SECONDS),
+        actualInstant.truncatedTo(ChronoUnit.SECONDS));
   }
 
   @Test
@@ -24,10 +48,31 @@ public class ApplicationUtilsTest {
 
     Duration d = new Duration();
     d.setUnit("min");
-    d.setValue(12345);
+    d.setValue(10);
 
-    Instant instant = ApplicationUtils.convertDurationToInstant(d);
-    assertNotNull(instant);
+    Instant expectedInstant = new Date().toInstant().plusSeconds(d.getValue().longValue() * 60);
+    Instant actualInstant = ApplicationUtils.convertDurationToInstant(d);
+
+    assertNotNull(actualInstant);
+    assertEquals(
+        expectedInstant.truncatedTo(ChronoUnit.MINUTES),
+        actualInstant.truncatedTo(ChronoUnit.MINUTES));
+  }
+
+  @Test
+  public void testConvertDurationToInstant_min_upperCase() {
+
+    Duration d = new Duration();
+    d.setUnit("MIN");
+    d.setValue(10);
+
+    Instant expectedInstant = new Date().toInstant().plusSeconds(d.getValue().longValue() * 60);
+    Instant actualInstant = ApplicationUtils.convertDurationToInstant(d);
+
+    assertNotNull(actualInstant);
+    assertEquals(
+        expectedInstant.truncatedTo(ChronoUnit.MINUTES),
+        actualInstant.truncatedTo(ChronoUnit.MINUTES));
   }
 
   @Test
@@ -35,9 +80,32 @@ public class ApplicationUtilsTest {
 
     Duration d = new Duration();
     d.setUnit("h");
-    d.setValue(12345);
+    d.setValue(1);
 
-    Instant instant = ApplicationUtils.convertDurationToInstant(d);
-    assertNotNull(instant);
+    Instant expectedInstant =
+        new Date().toInstant().plusSeconds(d.getValue().longValue() * 60 * 60);
+    Instant actualInstant = ApplicationUtils.convertDurationToInstant(d);
+
+    assertNotNull(actualInstant);
+    assertEquals(
+        expectedInstant.truncatedTo(ChronoUnit.MINUTES),
+        actualInstant.truncatedTo(ChronoUnit.MINUTES));
+  }
+
+  @Test
+  public void testConvertDurationToInstant_hour_upperCase() {
+
+    Duration d = new Duration();
+    d.setUnit("H");
+    d.setValue(1);
+
+    Instant expectedInstant =
+        new Date().toInstant().plusSeconds(d.getValue().longValue() * 60 * 60);
+    Instant actualInstant = ApplicationUtils.convertDurationToInstant(d);
+
+    assertNotNull(actualInstant);
+    assertEquals(
+        expectedInstant.truncatedTo(ChronoUnit.MINUTES),
+        actualInstant.truncatedTo(ChronoUnit.MINUTES));
   }
 }
