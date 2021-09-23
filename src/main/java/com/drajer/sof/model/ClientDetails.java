5e7b232cd3a582bcd9173c3ad2bf30e807597fd8
Kinny, Arvindo <Arvindo.Kinny@Cerner.com>
2021-09-23 10:56:07 -0400

    Add empty check before decrypting (#162)
    * Add empty check before decrypting
    
    * Add empty check in client details

--- src/main/java/com/drajer/sof/model/ClientDetails.java
+++ src/main/java/com/drajer/sof/model/ClientDetails.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.Type;
@@ -231,7 +232,7 @@ public class ClientDetails {
   }
 
   public String getDirectPwd() {
-    if (directPwd != null) {
+    if (directPwd != null && !StringUtils.isEmpty(directPwd)) {
       return AESEncryption.decrypt(directPwd);
     } else {
       return null;
--- src/main/java/com/drajer/sof/model/LaunchDetails.java
+++ src/main/java/com/drajer/sof/model/LaunchDetails.java
@@ -14,6 +14,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.Type;
@@ -395,7 +396,7 @@ public class LaunchDetails {
   }
 
   public String getDirectPwd() {
-    if (directPwd != null) {
+    if (directPwd != null && !StringUtils.isEmpty(directPwd)) {
       return AESEncryption.decrypt(directPwd);
     } else {
       return null;
