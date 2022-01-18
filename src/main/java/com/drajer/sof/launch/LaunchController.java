bd5a65223b7b790a59917f52f1d129d5f4f68c2f
sk094067 <sonu.kumar@cerner.com>
2022-01-18 15:40:01 +0530

    OPENLINK-7063 :In LaunchController.java inside getEncounterById logger changed from info to error for 4xx errors

--- src/main/java/com/drajer/sof/launch/LaunchController.java
+++ src/main/java/com/drajer/sof/launch/LaunchController.java
@@ -530,7 +530,8 @@ public class LaunchController {
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, EncounterError);
 
     } catch (ResourceNotFoundException notFoundException) {
-      logger.info(EncounterError, notFoundException);
+      // logger.info(EncounterError, notFoundException);
+      logger.error(EncounterError, notFoundException);
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, EncounterError, notFoundException);
 
     } catch (Exception e) {
