b9a2465e8d74959a91174cde5767d74c62bb3141
nbashyam <nagesh.bashyam@drajer.com>
2022-03-01 15:13:03 -0500

    Updates to Patient Launch Controller.

--- src/main/java/com/drajer/bsa/controller/PatientLaunchController.java
+++ src/main/java/com/drajer/bsa/controller/PatientLaunchController.java
@@ -10,12 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 /**
  * This controller is used to receive notifications from EHRs to launch specific patient instances
@@ -32,6 +30,19 @@ public class PatientLaunchController {
 
   private static final String FHIR_VERSION = "fhirVersion";
 
+  /**
+   * The method is the API to launch a patient instance within the app for processing. In addition
+   * to the parameters listed below, the following HTTP headers are expected to be populated.
+   * 'X-Request-ID' - This can be used for logging across the client and server. This is expected to
+   * be a GUID. 'X-Correlation-ID' - This can be used for correlation across clients and server
+   * request / responses. This is expected to be a GUID.
+   *
+   * @param launchContext - Contains the context of the launch.
+   * @param request - Request parameters
+   * @param response - Response parameters.
+   * @return
+   * @throws IOException
+   */
   @CrossOrigin
   @PostMapping(value = "/api/launchPatient")
   public String launchPatient(
@@ -47,16 +58,24 @@ public class PatientLaunchController {
         launchContext.getEncounterId(),
         request.getHeader("X-Request-ID"));
 
-    String requestIdHeadervalue = request.getHeader("X-Request-ID");
     HealthcareSetting hs = hsService.getHealthcareSettingByUrl(launchContext.getFhirServerURL());
 
-    if (hs != null) {
+    // If the healthcare setting exists
+    /*   if (hs != null) {
 
-    } else {
-      throw new ResponseStatusException(
-          HttpStatus.BAD_REQUEST, "Unrecognized healthcare setting FHIR URL ");
-    }
+       	String requestId = request.getHeader("X-Request-ID");
+           if(StringUtils.isEmpty(requestId)) {
+           	requestId = UUID.randomUUID().toString();
+           	request.
+           }
 
+           String correlationId = request.getHeader("X-Correlation-ID");
+
+       } else {
+         throw new ResponseStatusException(
+             HttpStatus.BAD_REQUEST, "Unrecognized healthcare setting FHIR URL ");
+       }
+    */
     return "Patient Instance launched for processing successfully";
   }
 }
