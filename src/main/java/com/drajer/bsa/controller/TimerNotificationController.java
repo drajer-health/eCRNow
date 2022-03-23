b76b9d3c645c838829b1bb6d3080c47b7c8abc5e
sk094067 <sonu.kumar@cerner.com>
2022-03-23 11:04:18 +0530

    ECRRPTG-176: sonar issue fixed

--- src/main/java/com/drajer/bsa/controller/TimerNotificationController.java
+++ src/main/java/com/drajer/bsa/controller/TimerNotificationController.java
@@ -1,3 +1,13 @@
 package com.drajer.bsa.controller;
 
-public class TimerNotificationController {}
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TimerNotificationController {
+
+  private final Logger logger = LoggerFactory.getLogger(TimerNotificationController.class);
+
+  public TimerNotificationController() {
+    logger.info("TimerNotificationController");
+  }
+}
