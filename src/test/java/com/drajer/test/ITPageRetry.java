10af43ab5be64118487b28c79e8ff8269a6f6fb3
sh097838 <sh097838@cerner.net>
2022-09-08 10:45:33 +0530

    Implemented test case for Page and Read queries

---
+++ src/test/java/com/drajer/test/ITPageRetry.java
@@ -0,0 +1,152 @@
+package com.drajer.test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.junit.Assert.*;
+
+import com.drajer.eca.model.PatientExecutionState;
+import com.drajer.ecrapp.model.Eicr;
+import com.drajer.sof.model.LaunchDetails;
+import com.drajer.test.util.TestDataGenerator;
+import com.drajer.test.util.WireMockHelper;
+import java.util.*;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RunWith(Parameterized.class)
+public class ITPageRetry extends BaseIntegrationTest {
+
+  private String testCaseId;
+  private Map<String, String> testData;
+  private Map<String, ?> allResourceMapping;
+  private Map<String, ?> allOtherMapping;
+  private List<Map<String, String>> fieldsToValidate;
+
+  public ITPageRetry(
+      String testCaseId,
+      Map<String, String> testData,
+      List<Map<String, String>> validateFields,
+      Map<String, ?> resourceMapping,
+      Map<String, ?> otherMapping) {
+
+    this.testCaseId = testCaseId;
+    this.testData = testData;
+    this.fieldsToValidate = validateFields;
+    this.allResourceMapping = resourceMapping;
+    this.allOtherMapping = otherMapping;
+  }
+
+  private static final Logger logger = LoggerFactory.getLogger(ITPageRetry.class);
+  private static String systemLaunchPayload;
+  private LaunchDetails launchDetails;
+  private PatientExecutionState state;
+  WireMockHelper stubHelper;
+
+  @Before
+  public void launchTestSetUp() throws Throwable {
+    logger.info("Executing Test: {}", testCaseId);
+    tx = session.beginTransaction();
+
+    // Data Setup
+    createClientDetails(testData.get("ClientDataToBeSaved"));
+    systemLaunchPayload = getSystemLaunchPayload(testData.get("SystemLaunchPayload"));
+    session.flush();
+    tx.commit();
+
+    stubHelper = new WireMockHelper(wireMockServer, wireMockHttpPort);
+    logger.info("Creating WireMock stubs..");
+    stubHelper.stubResources(allResourceMapping);
+    stubHelper.stubAuthAndMetadata(allOtherMapping);
+    mockRestApi();
+  }
+
+  @Parameterized.Parameters(name = "{0}")
+  public static Collection<Object[]> data() {
+
+    List<TestDataGenerator> testDataGenerator = new ArrayList<>();
+    testDataGenerator.add(new TestDataGenerator("test-yaml/fhirRetryPageTest.yaml"));
+
+    int totalTestCount = 0;
+    for (TestDataGenerator testData : testDataGenerator) {
+      totalTestCount = totalTestCount + testData.getAllTestCases().size();
+    }
+
+    Object[][] data = new Object[totalTestCount][5];
+
+    int count = 0;
+    for (TestDataGenerator testData : testDataGenerator) {
+      Set<String> testCaseSet = testData.getAllTestCases();
+      for (String testCase : testCaseSet) {
+        data[count][0] = testCase;
+        data[count][1] = testData.getTestCaseByID(testCase).getTestData();
+        data[count][2] = testData.getValidate(testCase);
+        data[count][3] = testData.getResourceMappings(testCase);
+        data[count][4] = testData.getOtherMappings(testCase);
+        count++;
+      }
+    }
+    return Arrays.asList(data);
+  }
+
+  @Test
+  public void testPage() throws Exception {
+
+    ResponseEntity<String> response = invokeSystemLaunch(testCaseId, systemLaunchPayload);
+
+    assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+    assertTrue(response.getBody().contains("App is launched successfully"));
+
+    logger.info("Received success response, waiting for EICR generation.....");
+    Eicr createEicr = getCreateEicrDocument();
+    wireMockServer.verify(
+        15,
+        getRequestedFor(
+            urlEqualTo(
+                "/FHIR/Observation?patient=12742571&category=laboratory&-pageContext=10065315_11316911_12742571_1_1&-pageDirection=NEXT")));
+  }
+
+  private Eicr getCreateEicrDocument() {
+    try {
+      do {
+        // Minimum 2 sec is required as App will execute
+        // createEicr workflow after 2 sec as per eRSD.
+        Thread.sleep(2000);
+        getLaunchDetailAndStatus();
+
+      } while (!state.getCreateEicrStatus().getEicrCreated());
+
+      return (session.get(
+          Eicr.class,
+          Integer.parseInt(
+              state.getCreateEicrStatus() != null ? state.getCreateEicrStatus().geteICRId() : "")));
+
+    } catch (Exception e) {
+      logger.error("Exception retrieving EICR ", e);
+      fail("Something went wrong retrieving EICR, check the log");
+    }
+    return null;
+  }
+
+  private void getLaunchDetailAndStatus() {
+    try {
+      Criteria criteria = session.createCriteria(LaunchDetails.class);
+      criteria.add(Restrictions.eq("xRequestId", testCaseId));
+      launchDetails = (LaunchDetails) criteria.uniqueResult();
+
+      state = mapper.readValue(launchDetails.getStatus(), PatientExecutionState.class);
+      session.refresh(launchDetails);
+
+    } catch (Exception e) {
+      logger.error("Exception occurred retrieving launchDetail and status", e);
+      fail("Something went wrong with launch status, check the log");
+    }
+  }
+}
---
+++ src/test/java/com/drajer/test/ITRetryLogic.java
@@ -0,0 +1,98 @@
+package com.drajer.test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.drajer.eca.model.PatientExecutionState;
+import com.drajer.sof.model.LaunchDetails;
+import com.drajer.test.util.TestDataGenerator;
+import com.drajer.test.util.TestUtils;
+import com.drajer.test.util.WireMockHelper;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RunWith(Parameterized.class)
+public class ITRetryLogic extends BaseIntegrationTest {
+
+  private String testCaseId;
+  private Map<String, String> testData;
+  private Map<String, ?> allResourceMapping;
+  private Map<String, ?> allOtherMapping;
+
+  public ITRetryLogic(
+      String testCaseId,
+      Map<String, String> testData,
+      Map<String, ?> resourceMapping,
+      Map<String, ?> otherMapping) {
+    this.testCaseId = testCaseId;
+    this.testData = testData;
+    this.allResourceMapping = resourceMapping;
+    this.allOtherMapping = otherMapping;
+  }
+
+  private static final Logger logger = LoggerFactory.getLogger(ITRetryLogic.class);
+  private String systemLaunchPayLoad;
+  private LaunchDetails launchDetails;
+  private PatientExecutionState state;
+  WireMockHelper stubHelper;
+
+  @Before
+  public void launchTestSetUp() throws IOException {
+    logger.info("Executing test: {}", testCaseId);
+    tx = session.beginTransaction();
+
+    // Data Setup
+    createClientDetails(testData.get("ClientDataToBeSaved"));
+    systemLaunchPayLoad = getSystemLaunchPayload(testData.get("SystemLaunchPayload"));
+    session.flush();
+    tx.commit();
+
+    stubHelper = new WireMockHelper(wireMockServer, wireMockHttpPort);
+    logger.info("Creating WireMock stubs..");
+    stubHelper.stubResources(allResourceMapping);
+    stubHelper.stubAuthAndMetadata(allOtherMapping);
+    mockRestApi();
+
+    String response = TestUtils.getFileContentAsString("R4/Patient/Patient_12742571.json");
+    wireMockServer.stubFor(
+        post(urlEqualTo("http://localhost:9010/FHIR/Encounter?patient=12742571"))
+            .atPriority(1)
+            .willReturn(aResponse().withStatus(202).withBody(response)));
+  }
+
+  @Parameterized.Parameters(name = "{0}")
+  public static Collection<Object[]> data() {
+    TestDataGenerator testDataGenerator = new TestDataGenerator("test-yaml/fhirRetryReadTest.yaml");
+
+    Set<String> testCaseSet = testDataGenerator.getAllTestCases();
+    Object[][] data = new Object[testCaseSet.size()][4];
+
+    int count = 0;
+    for (String testCase : testCaseSet) {
+      data[count][0] = testCase;
+      data[count][1] = testDataGenerator.getTestCaseByID(testCase).getTestData();
+      data[count][2] = testDataGenerator.getResourceMappings(testCase);
+      data[count][3] = testDataGenerator.getOtherMappings(testCase);
+      count++;
+    }
+    return Arrays.asList(data);
+  }
+
+  @Test
+  public void testFhirRetryUrl() {
+    ResponseEntity<String> response = invokeSystemLaunch(testCaseId, systemLaunchPayLoad);
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    wireMockServer.verify(5, getRequestedFor(urlEqualTo("/FHIR/Encounter/97953900")));
+  }
+}
