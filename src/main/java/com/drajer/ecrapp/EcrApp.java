95821641edafa00167c432466c96dc48718f2d61
annamdas <thirupathi.annamdas@xyramsoft.com>
2023-01-23 15:49:16 +0530

    added Rest Template pooling connection configuration

--- src/main/java/com/drajer/ecrapp/EcrApp.java
+++ src/main/java/com/drajer/ecrapp/EcrApp.java
@@ -4,6 +4,8 @@ import com.drajer.ecrapp.security.AuthorizationService;
 import com.drajer.ecrapp.security.RequestMDCFilter;
 import com.drajer.ecrapp.security.SampleAuthorizationServiceImpl;
 import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -12,6 +14,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +22,12 @@ import org.springframework.web.client.RestTemplate;
 @EnableTransactionManagement
 @ServletComponentScan
 public class EcrApp extends SpringBootServletInitializer {
+
+  @Value("${rest.template.connection.timeout}")
+  private Integer connectionTimeOut;
+
+  @Value("${rest.template.read.timeout}")
+  private Integer readTimeout;
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -32,7 +41,11 @@ public class EcrApp extends SpringBootServletInitializer {
 
   @Bean
   public RestTemplate restTemplate() {
-    return new RestTemplate();
+    SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+    clientHttpRequestFactory.setConnectTimeout(connectionTimeOut);
+    clientHttpRequestFactory.setReadTimeout(readTimeout);
+    RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
+    return restTemplate;
   }
 
   @Bean
