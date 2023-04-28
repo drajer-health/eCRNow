d255a7e40217970aaca588525c07831a42c63a34
Bhuvan Damodhar <Bhuvan.Damodhar2@cerner.com>
2023-04-28 08:46:23 -0500

    Create trigger (#270)
    * Fix to trigger eICR
    
    Fix to trigger eICR even when trigger match status is true on previous match trigger check
    
    * fixed
    
    * adding import

--- src/main/java/com/drajer/eca/model/CreateEicrAction.java
+++ src/main/java/com/drajer/eca/model/CreateEicrAction.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 @Service
 public class CreateEicrAction extends AbstractAction {
