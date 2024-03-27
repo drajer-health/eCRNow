6636d9d2b33aed040082a4d36c2fafa7d8ac11fb
bhargav k <bhargav.chowdhary@xyramsoft.com>
2024-03-28 00:16:26 +0530

    updated healthcare_setting.sql script

--- src/main/resources/db-scripts/healthcare_setting.sql
+++ src/main/resources/db-scripts/healthcare_setting.sql
@@ -1,5 +1,5 @@
 ALTER TABLE healthcare_setting
-  ADD COLUMN smtp_tls_version VARCHAR(256),
+  ADD COLUMN smtp_tls_version VARCHAR(256) DEFAULT 'TLSv1.2',
    ADD debug_enabled BIT DEFAULT CAST(1 AS BIT);
 
 ALTER TABLE healthcare_setting
