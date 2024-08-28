ac3203f5a90fa50ad6a1546eb079986740d47ab1
Chris O Riordan <chris.oriordan@smilecdr.com>
2024-08-28 15:12:51 -0700

    Update Diabetes Content to 2024 Spec and modify test to reflect.
    -Update some test config values to reflect change in port from 9011 to 9010

--- src/test/java/com/drajer/test/BaseIntegrationTest.java
+++ src/test/java/com/drajer/test/BaseIntegrationTest.java
@@ -67,7 +67,7 @@ public abstract class BaseIntegrationTest {
   protected static final String URL = "http://localhost:";
   protected static final String fhirBaseUrl = "/FHIR";
 
-  protected static final int wireMockHttpPort = 9011;
+  protected static final int wireMockHttpPort = 9010;
   protected WireMockServer wireMockServer;
   protected ClientDetails clientDetails;
 
