fe344d53f1d7e8aaaabcf240d832a29cf4ccf419
bhargav <bhargav.chowdhary@xyramsoft.com>
2024-09-09 16:56:16 +0530

    resloved conflict

--- src/main/java/com/drajer/bsa/controller/TimeZoneController.java
+++ src/main/java/com/drajer/bsa/controller/TimeZoneController.java
@@ -2,7 +2,11 @@ package com.drajer.bsa.controller;
 
 import com.drajer.bsa.service.TimeZoneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +17,16 @@ public class TimeZoneController {
   @GetMapping("/api/timezone")
   public String getDatabaseTimezone() {
     return timeZoneService.getDatabaseTimezone();
+  }
+
+  @PostMapping("/api/timezone")
+  public ResponseEntity<String> setTimeZone(@RequestParam String timeZone) {
+    try {
+      timeZoneService.setDatabaseTimezone(timeZone);
+      return ResponseEntity.ok("Time zone updated successfully to " + timeZone);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("Error updating time zone: " + e.getMessage());
+    }
   }
 }
