912e22bd9e9104723271096b1c4015c64be8a88d
bhargav <bhargav.chowdhary@xyramsoft.com>
2025-01-13 23:04:19 +0530

    key cloak implemetaion

--- src/main/java/com/drajer/ecrapp/security/TokenFilter.java
+++ src/main/java/com/drajer/ecrapp/security/TokenFilter.java
@@ -48,9 +48,8 @@ public class TokenFilter extends OncePerRequestFilter {
 
       chain.doFilter(request, response);
     } else {
-      //      log.error("Access token validation failed.");
-      //      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-      chain.doFilter(request, response);
+      log.error("Access token validation failed.");
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
   }
 
