03a68acc988c508c6f4bfc3cac44e4e395a41101
bhargav <bhargav.chowdhary@xyramsoft.com>
2025-01-20 17:46:20 +0530

    update

---
+++ src/main/java/com/drajer/ecrapp/exceptions/KeycloakCredentialsException.java
@@ -0,0 +1,7 @@
+package com.drajer.ecrapp.exceptions;
+
+public class KeycloakCredentialsException extends RuntimeException {
+  public KeycloakCredentialsException(String message) {
+    super(message);
+  }
+}
