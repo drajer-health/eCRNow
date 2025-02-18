47ec658f38160d64536d319ce8ec29a35b620cc7
bhargav <bhargav.chowdhary@xyramsoft.com>
2025-02-18 18:12:58 +0530

    cros fixes

--- src/main/java/com/drajer/ecrapp/controller/KeycloakTokenController.java
+++ src/main/java/com/drajer/ecrapp/controller/KeycloakTokenController.java
@@ -20,6 +20,7 @@ public class KeycloakTokenController {
    * @param tokenDetails Map containing token details like client_id, client_secret, etc.
    * @return ResponseEntity indicating success or failure of token generation.
    */
+  @CrossOrigin
   @PostMapping("/generate-token")
   public ResponseEntity<Object> generateToken(@RequestParam Map<String, Object> tokenDetails) {
     if (tokenDetails == null || tokenDetails.isEmpty()) {
@@ -28,6 +29,35 @@ public class KeycloakTokenController {
 
     try {
       Object tokenResponse = keyCloakTokenValidationClient.generateToken(tokenDetails);
+      if (tokenResponse != null) {
+        return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(tokenResponse.toString());
+
+      } else {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token validation failed.");
+      }
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An error occurred while validating the token: " + e.getMessage());
+    }
+  }
+  /**
+   * Endpoint to generate and validate a token.
+   *
+   * @param tokenDetails Map containing token details like client_id, client_secret, etc.
+   * @return ResponseEntity indicating success or failure of token generation.
+   */
+  @CrossOrigin
+  @PostMapping("/generateAuthToken")
+  public ResponseEntity<Object> generateUserAuthToken(
+      @RequestParam Map<String, Object> tokenDetails) {
+    if (tokenDetails == null || tokenDetails.isEmpty()) {
+      return ResponseEntity.badRequest().body("Token details are required.");
+    }
+
+    try {
+      Object tokenResponse = keyCloakTokenValidationClient.generateUserAuthToken(tokenDetails);
       if (tokenResponse != null) {
         return ResponseEntity.ok()
             .contentType(MediaType.APPLICATION_JSON)
--- src/main/java/com/drajer/ecrapp/security/KeyCloakTokenValidationClient.java
+++ src/main/java/com/drajer/ecrapp/security/KeyCloakTokenValidationClient.java
@@ -138,6 +138,66 @@ public class KeyCloakTokenValidationClient {
     }
   }
 
+  public Object generateUserAuthToken(Map<String, Object> authenciationTokenDetails)
+      throws IOException {
+    LOGGER.info("Entry - Generating Token Method in KeyCloakTokenValidationClient");
+
+    fetchAndValidateKeyCredentials();
+
+    String url = String.format("%s/realms/%s/protocol/openid-connect/token", authUrl, realm);
+
+    MediaType mediaType = MediaType.parse(APPLICATION_URL_FORM_ENCODED);
+
+    StringBuilder authRequestBody = new StringBuilder();
+
+    authenciationTokenDetails.put("client_id", clientId);
+    authenciationTokenDetails.put("client_secret", clientSecret);
+    authenciationTokenDetails.put("grant_type", "password");
+
+    for (Map.Entry<String, Object> authEntry : authenciationTokenDetails.entrySet()) {
+      authRequestBody
+          .append(authEntry.getKey())
+          .append("=")
+          .append(authEntry.getValue())
+          .append("&");
+    }
+    if (authRequestBody.length() > 0) {
+
+      authRequestBody.setLength(authRequestBody.length() - 1);
+    }
+    RequestBody body = RequestBody.create(mediaType, authRequestBody.toString());
+
+    Request requestOne =
+        new Request.Builder()
+            .url(url)
+            .method("POST", body)
+            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+            .build();
+
+    try (Response response = client.newCall(requestOne).execute()) {
+
+      if (!response.isSuccessful()) {
+        LOGGER.error("Failed to authenticate: {}", response.message());
+        return false;
+      }
+      System.out.println(response);
+
+      String responseBody = response.body() != null ? response.body().string() : "{}";
+      System.out.println(responseBody);
+      return new JSONObject(responseBody);
+
+    } catch (IOException e) {
+      LOGGER.error("Exception - validateToken Method in KeyCloakTokenValidationClient", e);
+      throw new IOException("Exception - validateToken Method in KeyCloakTokenValidationClient", e);
+    } catch (Exception e) {
+
+      throw new KeycloakCredentialsException(
+          "Exception - generate Method in KeyCloakTokenValidationClient" + e.getMessage());
+    } finally {
+      LOGGER.info("Exit - validateToken Method in KeyCloakTokenValidationClient");
+    }
+  }
+
   private String getTokenFromRequest(HttpServletRequest request) {
     String authorizationHeaderValue = request.getHeader(AUTHORIZATION_HEADER);
     if (authorizationHeaderValue != null && authorizationHeaderValue.startsWith(BEARER_PREFIX)) {
