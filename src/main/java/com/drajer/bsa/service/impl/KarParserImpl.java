721e51acf0caef02088f0b5a942a9e99320e2ebc
Chris O Riordan <chris.oriordan@smilecdr.com>
2025-02-26 10:07:29 -0800

    Prevent duplicate resources from being added to InMemoryFhirRepository

--- src/main/java/com/drajer/bsa/service/impl/KarParserImpl.java
+++ src/main/java/com/drajer/bsa/service/impl/KarParserImpl.java
@@ -62,6 +62,7 @@ import org.hl7.fhir.r4.model.Expression;
 import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Library;
+import org.hl7.fhir.r4.model.Measure;
 import org.hl7.fhir.r4.model.PlanDefinition;
 import org.hl7.fhir.r4.model.PlanDefinition.ActionRelationshipType;
 import org.hl7.fhir.r4.model.PlanDefinition.PlanDefinitionActionComponent;
@@ -80,6 +81,7 @@ import org.opencds.cqf.fhir.cr.cpg.r4.R4CqlExecutionService;
 import org.opencds.cqf.fhir.cr.cpg.r4.R4LibraryEvaluationService;
 import org.opencds.cqf.fhir.cr.measure.r4.R4MeasureService;
 import org.opencds.cqf.fhir.utility.repository.InMemoryFhirRepository;
+import org.opencds.cqf.fhir.utility.search.Searches;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
@@ -490,11 +492,28 @@ public class KarParserImpl implements KarParser {
 
   /**
    * Method to insert Kar Resources into inMemoryFhirRepository, for measure processing.
+   * Resources are only inserted if they are not already present.
    *
    * @param resource the resource to store.
    */
   private void addKarResourceToFhirRepository(Resource resource) {
-    this.repository.create(resource);
+    Bundle bundle;
+    if (resource instanceof Library library) {
+      bundle = this.repository.search(Bundle.class, library.getClass(), Searches.byUrlAndVersion(library.getUrl(), library.getVersion()));
+    } else if (resource instanceof PlanDefinition planDef) {
+      bundle = this.repository.search(Bundle.class, planDef.getClass(), Searches.byUrlAndVersion(planDef.getUrl(), planDef.getVersion()));
+    } else if (resource instanceof Measure measure) {
+      bundle = this.repository.search(Bundle.class, measure.getClass(), Searches.byUrlAndVersion(measure.getUrl(), measure.getVersion()));
+    } else if (resource instanceof ValueSet valueSet) {
+      bundle = this.repository.search(Bundle.class, valueSet.getClass(), Searches.byUrlAndVersion(valueSet.getUrl(), valueSet.getVersion()));
+    } else {
+      logger.info("Unexpected Resource Type - attempting to retrieve by ID");
+      bundle = this.repository.search(Bundle.class, resource.getClass(), Searches.byId(resource.getId()));
+    }
+
+    if (!bundle.hasEntry()) {
+      this.repository.create(resource);
+    }
   }
 
   /**
