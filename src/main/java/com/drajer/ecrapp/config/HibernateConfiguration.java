edc1ee981e42e087d40556cca0a2c5322e815401
bhargavchowdhary <70563417+bhargavchowdhary@users.noreply.github.com>
2025-03-19 23:59:33 +0530

    resloved keycloak conflict3 on rebase

--- src/main/java/com/drajer/ecrapp/config/HibernateConfiguration.java
+++ src/main/java/com/drajer/ecrapp/config/HibernateConfiguration.java
@@ -59,7 +59,7 @@ public class HibernateConfiguration {
 
   private Properties hibernateProperties() {
     Properties properties = new Properties();
-    // properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+    properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
     properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
     properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
     properties.put(
@@ -71,6 +71,7 @@ public class HibernateConfiguration {
   }
 
   @Bean
+  @Autowired
   public HibernateTransactionManager transactionManager(SessionFactory s) {
     HibernateTransactionManager txManager = new HibernateTransactionManager();
     txManager.setSessionFactory(s);
