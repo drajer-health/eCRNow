230a6d2c2b918ec641efe60e36ec4e182542893f
bhargavchowdhary <70563417+bhargavchowdhary@users.noreply.github.com>
2025-04-03 17:28:52 +0530

    cors fixes and keycloak response and dependency fixes

--- src/main/java/com/drajer/ecrapp/security/KeyCloakTokenValidationClient.java
+++ src/main/java/com/drajer/ecrapp/security/KeyCloakTokenValidationClient.java
@@ -113,13 +113,16 @@ public class KeyCloakTokenValidationClient {
 
     try (Response response = client.newCall(requestOne).execute()) {
 
+      String responseBody = response.body() != null ? response.body().string() : "{}";
+      JSONObject keyCloakResponse = new JSONObject(responseBody);
+
       if (!response.isSuccessful()) {
         LOGGER.error("Failed to authenticate: {}", response.message());
-        return false;
+        keyCloakResponse.put("isSuccess", false);
+        return keyCloakResponse;
       }
-
-      String responseBody = response.body() != null ? response.body().string() : "{}";
-      return new JSONObject(responseBody);
+      keyCloakResponse.put("isSuccess", true);
+      return keyCloakResponse;
 
     } catch (IOException e) {
       LOGGER.error("Exception - validateToken Method in KeyCloakTokenValidationClient", e);
