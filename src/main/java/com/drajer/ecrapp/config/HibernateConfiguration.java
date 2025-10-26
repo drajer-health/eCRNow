d8494ce9c11d174dba38263dc0aae4e3f4019540
bhargavchowdhary <bhargav.chowdhary@xyramsoft.com>
2025-10-27 00:13:31 +0530

    Fixed merge conflicts during rebase with master branch

--- src/main/java/com/drajer/ecrapp/config/HibernateConfiguration.java
+++ src/main/java/com/drajer/ecrapp/config/HibernateConfiguration.java
@@ -59,7 +59,7 @@ public class HibernateConfiguration {
 
   private Properties hibernateProperties() {
     Properties properties = new Properties();
-     properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+    properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
     properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
     properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
     properties.put(
