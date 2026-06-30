80b2dcdecdd67c04485902e257f43573568c822e
bhargavchowdhary <bhargav.chowdhary@xyramsoft.com>
2026-06-30 22:58:27 +0530

    format-issues

--- src/test/java/com/drajer/cdafromr4/CdaPregnancyGeneratorTest.java
+++ src/test/java/com/drajer/cdafromr4/CdaPregnancyGeneratorTest.java
@@ -1,6 +1,5 @@
 package com.drajer.cdafromr4;
 
-import static com.helger.commons.mock.CommonsAssert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
 import com.drajer.bsa.utils.R3ToR2DataConverterUtils;
