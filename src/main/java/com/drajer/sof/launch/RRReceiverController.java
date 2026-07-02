a3aea539a58a2e94aa708f3d67165c022f0defd2
bhargavchowdhary <bhargav.chowdhary@xyramsoft.com>
2026-07-02 14:42:03 +0530

    unit test fixes

--- src/main/java/com/drajer/sof/launch/RRReceiverController.java
+++ src/main/java/com/drajer/sof/launch/RRReceiverController.java
@@ -3,7 +3,6 @@ package com.drajer.sof.launch;
 import com.drajer.ecrapp.model.Eicr;
 import com.drajer.ecrapp.model.ReportabilityResponse;
 import com.drajer.ecrapp.service.EicrRRService;
-import com.drajer.sof.utils.SecurityValidationUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
@@ -48,12 +47,6 @@ public class RRReceiverController {
           logger.error("X-Correlation-ID header is not present in MDN request.");
           throw new ResponseStatusException(
               HttpStatus.BAD_REQUEST, "X-Correlation-ID header is not present in MDN request.");
-        }
-        if (StringUtils.isBlank(data.getFhirUrl())
-            || !SecurityValidationUtils.validateUrl(data.getFhirUrl())) {
-          logger.error("FHIR URL is not present or invalid in MDN request.");
-          throw new ResponseStatusException(
-              HttpStatus.BAD_REQUEST, "FHIR URL is not present or invalid in MDN request.");
         }
 
         // For MDN, no other data will be present.
